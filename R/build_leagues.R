5379f3557e580b8cc8165e5c13cbef62c72efb1e
dujmi <dujakmislav@gmail.com>
2024-11-02 21:36:46 +0100

    Added build_leagues

---
+++ R/build_leagues.R
@@ -0,0 +1,17 @@
+build_leagues <- function() {
+    leagues <- tidytable(
+        name = c("Premier League", "La Liga", "Serie A", "Ligue 1", "Bundesliga"),
+        fb_url = c(
+            "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures",
+            "https://fbref.com/en/comps/12/schedule/La-Liga-Scores-and-Fixtures",
+            "https://fbref.com/en/comps/11/schedule/Serie-A-Scores-and-Fixtures",
+            "https://fbref.com/en/comps/13/schedule/Ligue-1-Scores-and-Fixtures",
+            "https://fbref.com/en/comps/20/schedule/Bundesliga-Scores-and-Fixtures"
+        ),
+        fdck_id = c("E0", "SP1", "I1", "F1", "D1"),
+        format = c(2, 2, 2, 2, 2)
+    )
+
+    saveRDS(leagues, file = "data/input/clean/leagues.rds")
+    return leagues
+}
--- R/model.R
+++ R/model.R
@@ -1,1 +1,17 @@
-# model.R
+if (!file.exists("data/input/leagues.rds")) {
+    leagues <- build_leagues()
+}
+else {
+    leagues <- readRDS("data/input/leagues.rds")
+}
+
+seasons <- c(23, 24) # start year
+
+for (i in 1:nrow(leagues)) {
+    league = leagues[i, ]
+
+    for (season in seasons) {
+        
+    }
+}
+
