7e17eba3e402f21802a9ad050e436236288031ad
dujmi <dujakmislav@gmail.com>
2024-12-23 21:53:16 +0100

    add loading of local files for completed seasons

--- R/load_league_odds.R
+++ R/load_league_odds.R
@@ -1,12 +1,18 @@
 load_league_odds <- function(seasons, fdck_id, name) {
     odds <- NULL
     for (season in seasons) {
-        season_odds <- tidytable::tidytable(readr::read_csv(
-            sprintf(
+        if (season == seasons[2]) {
+            path <- sprintf(
                 "https://www.football-data.co.uk/mmz4281/%d%d/%s.csv",
                 season, season + 1, fdck_id
             )
-        )) |>
+        } else {
+            path <- sprintf(
+                "data/input/%s.csv",
+                fdck_id
+            )
+        }
+        season_odds <- tidytable::tidytable(readr::read_csv(path)) |>
             tidytable::select(Date, Time, HomeTeam, AwayTeam, AvgCH, AvgCD, AvgCA) |>
             tidytable::mutate(
                 competition = name,
